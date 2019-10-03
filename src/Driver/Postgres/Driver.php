bf482fe6bf8a1533f28b2fb7cb5954e6bb695ca8
Andrey Borodulin <borodulin@gmail.com>
2019-10-03 23:07:32 +0500

    refactoring

--- src/Driver/Postgres/Driver.php
+++ src/Driver/Postgres/Driver.php
@@ -9,16 +9,16 @@ use FastOrm\Driver\SavepointInterface;
 use FastOrm\Driver\SavepointTrait;
 use FastOrm\SQL\Compiler;
 use FastOrm\SQL\CompilerInterface;
-use FastOrm\SQL\ParamsInterface;
+use FastOrm\SQL\ContextInterface;
 use FastOrm\SQL\SearchCondition\Operator\LikeOperator;
 
 class Driver extends AbstractDriver implements SavepointInterface
 {
     use SavepointTrait;
 
-    public function createCompiler(ParamsInterface $params): CompilerInterface
+    public function createCompiler(ContextInterface $context): CompilerInterface
     {
-        return new Compiler($params, [
+        return new Compiler($context, [
             LikeOperator::class => LikeOperatorBuilder::class,
         ]);
     }
--- src/SQL/Clause/InsertQuery.php
+++ src/SQL/Clause/InsertQuery.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
 
 namespace FastOrm\SQL\Clause;
 
 
-interface UpdateInterface
+class InsertQuery
 {
 
 }
